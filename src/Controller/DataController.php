5ff025d821aa9c2e2bf0ca15d82f90f72d259f0d
Adam <adam.wozniak94@hotmail.com>
2022-10-12 20:31:06 +0200

    add generateRaport functionality

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -6,6 +6,7 @@ use App\Entity\Data;
 use App\Form\CsvType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -42,8 +43,43 @@ class DataController extends AbstractController
     }
 
     #[Route('/raport', name: 'app_raport')]
-    public function generateRaport(Request $request)
+    public function generateRaport(Request $request, ManagerRegistry $doctrine)
     {
+        $dataRepository = $doctrine->getRepository(Data::class);
+        $version = $request->get('version');
+        $raportDate = $request->get('raport-date');
 
+        $fileName = 'Raport' . $version . '.csv';
+        if (1 == $version) {
+            $fileHeaders = [
+                'Nr rozliczeniowy',
+                'Imię i Nazwisko',
+                'Status Zamówienia',
+                'Liczba zamówień',
+                'Suma kosztów',
+                'Unikalne nazwy ofert'
+            ];
+            $results = $dataRepository->getRaport1Data($raportDate);
+        } else {
+            $fileHeaders = [
+                'Miesiąc',
+                'Czy Upgrade',
+                'Nazwa oferty',
+                'Liczba zamówień'
+            ];
+            $results = $dataRepository->getRaport2Data($raportDate);
+        }
+
+        $rows = [];
+        $rows[] = implode(';', $fileHeaders);
+        foreach ($results as $result) {
+            $rows[] = implode(';', $result);
+        }
+        $content = implode("\n", $rows);
+        $response = new Response($content);
+        $response->headers->set('Content-Type', 'text/csv; charset=utf-8');
+        $response->headers->set('Content-Disposition', 'attachment; filename=' . $fileName);
+
+        return $response;
     }
 }
